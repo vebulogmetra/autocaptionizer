581f067ca2266651c0f54139e38ba65e0b7d0caa
Artem <vebulogmetra@yandex.ru>
2024-02-04 16:47:44 +0300

    remove uvivorn worcers count from setting

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -13,7 +13,6 @@ class Config:
     base_dir: PathLike = Path(__file__).parent.parent.parent  # autocaptionizer
     app_host: str = "127.0.0.1"
     app_port: int = 8000
-    app_workers_count: int = 1
     app_version: str = "0.1.1"
     pretrained_model_name: str = "Salesforce/blip-image-captioning-large"
     conditional_text: str = "a photography of"
